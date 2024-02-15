e08cf87e723a2d0f3229b0af809e5f13f85cad5f
Crowdbotics <team@crowdbotics.com>
2024-02-15 21:51:31 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ai-powered-image-46384",
+  url: "https://ai-powered-image-46384.botics.co",
+  api: "https://ai-powered-image-46384.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
